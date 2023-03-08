2962a0af347173b195a646068668bd1d23488c6c
parixscm <parixscm@gmail.com>
2023-03-09 00:18:11 +0900

    💄 Design: DELETE scrollbar from stories

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { User } from "../typings";
 import { createRandomUser } from "../libs/createRandomUser";
+import Story from "./Story";
 
 function Stories() {
   const [suggestions, setSuggestions] = useState<User[]>([]);
@@ -13,10 +14,10 @@ function Stories() {
   }, []);
 
   return (
-    <div>
-      {/* {suggestions.map(({ id, username, avatar }) => (
+    <div className="mt-8 p-6 flex space-x-2 bg-white border border-gray-200 rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {suggestions.map(({ id, username, avatar }) => (
         <Story key={id} username={username} img={avatar} />
-      ))} */}
+      ))}
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>Instagram Clone</title>
         <link rel="icon" href="/favicon.ico" />
