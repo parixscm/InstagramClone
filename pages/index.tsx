beaa94eaf0896a8949ff7746ea68e63d16c92028
parixscm <parixscm@gmail.com>
2023-03-08 19:30:23 +0900

    🎉 Feat: ADD initial project files

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Instagram Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Header */}
+
+      {/* Feed */}
+
+      {/* Modal */}
+    </div>
+  );
+};
+
+export default Home;
