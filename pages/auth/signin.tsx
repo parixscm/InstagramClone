e89983a416c365e420b27a362bf3122fcdbcd297
parixscm <parixscm@gmail.com>
2023-03-11 00:35:49 +0900

    📝 Feat: ADD custom signin page with ssr configuration

---
+++ pages/auth/signin.tsx
@@ -0,0 +1,41 @@
+import {
+  ClientSafeProvider,
+  getProviders,
+  LiteralUnion,
+  signIn,
+} from "next-auth/react";
+import { BuiltInProviderType } from "next-auth/providers";
+
+function signInPage({
+  providers,
+}: {
+  providers: Record<
+    LiteralUnion<BuiltInProviderType, string>,
+    ClientSafeProvider
+  >;
+}) {
+  console.log(providers);
+
+  return (
+    <>
+      {Object.values(providers).map(provider => (
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+}
+
+export default signInPage;
