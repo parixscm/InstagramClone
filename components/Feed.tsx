66e8ece5054688fc997f0406024c1835a6016b38
parixscm <parixscm@gmail.com>
2023-03-11 17:24:33 +0900

    💄 Feat: ADD session to feed, stories

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,11 +1,18 @@
+import { useSession } from "next-auth/react";
 import Stories from "./Stories";
 import Posts from "./Posts";
 import MiniProfile from "./MiniProfile";
 import Suggestions from "./Suggestions";
 
 function Feed() {
+  const { data: session } = useSession();
+
   return (
-    <main className="mx-auto grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl">
+    <main
+      className={`mx-auto grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl ${
+        !session && "!grid-cols-1 !max-w-3xl"
+      }`}
+    >
       <section className="col-span-2">
         {/* Stories */}
         <Stories />
@@ -14,15 +21,17 @@ function Feed() {
         <Posts />
       </section>
 
-      <section className="hidden md:grid-cols-1 xl:inline-grid">
-        <div className="fixed top-20">
-          {/* Mini Profile */}
-          <MiniProfile />
+      {session && (
+        <section className="hidden md:grid-cols-1 xl:inline-grid">
+          <div className="fixed top-20">
+            {/* Mini Profile */}
+            <MiniProfile />
 
-          {/* Suggestions */}
-          <Suggestions />
-        </div>
-      </section>
+            {/* Suggestions */}
+            <Suggestions />
+          </div>
+        </section>
+      )}
     </main>
   );
 }
--- components/Stories.tsx
+++ components/Stories.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import { User } from "../typings";
 import { createRandomUser } from "../libs/createRandomUser";
 import Story from "./Story";
+import { useSession } from "next-auth/react";
 
 function Stories() {
   const [suggestions, setSuggestions] = useState<User[]>([]);
+  const { data: session } = useSession();
 
   useEffect(() => {
     const suggestions = [...Array(20)].map(() => ({
@@ -15,6 +17,13 @@ function Stories() {
 
   return (
     <div className="mt-8 p-6 flex space-x-2 bg-white border border-gray-200 rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {session && (
+        <Story
+          username={session?.user?.username!}
+          img={session?.user?.image!}
+        />
+      )}
+
       {suggestions.map(({ id, username, avatar }) => (
         <Story key={id} username={username} img={avatar} />
       ))}
