4903b3f8d2a7a7b88dd061fe732f258d18df4f65
parixscm <parixscm@gmail.com>
2023-03-13 15:32:40 +0900

    🎨 Fix: ADD real time comments data from firebase database

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { IPost } from "../typings";
 import {
   HeartIcon,
@@ -8,8 +9,50 @@ import {
   ChatBubbleLeftEllipsisIcon,
 } from "@heroicons/react/24/outline";
 import { HeartIcon as FilledHeartIcon } from "@heroicons/react/24/solid";
+import { useSession } from "next-auth/react";
+import {
+  addDoc,
+  serverTimestamp,
+  onSnapshot,
+  collection,
+  query,
+  orderBy,
+  QuerySnapshot,
+  DocumentData,
+  QueryDocumentSnapshot,
+} from "firebase/firestore";
+import { db } from "../firebase";
 
 function Post({ id, username, userImg, postImg, caption }: IPost) {
+  const { data: session } = useSession();
+  const [comment, setComment] = useState("");
+  const [comments, setComments] =
+    useState<QueryDocumentSnapshot<DocumentData>[]>();
+
+  const sendComment = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const commentToSend = comment;
+    setComment("");
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: session?.user.username,
+      userImage: session?.user.image,
+      timestamp: serverTimestamp(),
+    });
+  };
+
+  useEffect(() => {
+    onSnapshot(
+      query(
+        collection(db, "posts", id, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot: QuerySnapshot<DocumentData>) => {
+        setComments(snapshot.docs);
+      }
+    );
+  }, [db]);
+
   return (
     <div className="my-7 bg-white border rounded-sm">
       {/* Header */}
@@ -27,14 +70,16 @@ function Post({ id, username, userImg, postImg, caption }: IPost) {
       <img src={postImg} className="w-full object-cover" />
 
       {/* Buttons */}
-      <div className="px-4 pt-4 flex justify-between">
-        <div className="flex space-x-4">
-          <HeartIcon className="postBtn" />
-          <ChatBubbleLeftEllipsisIcon className="postBtn" />
-          <PaperAirplaneIcon className="postBtn" />
+      {session && (
+        <div className="px-4 pt-4 flex justify-between">
+          <div className="flex space-x-4">
+            <HeartIcon className="postBtn" />
+            <ChatBubbleLeftEllipsisIcon className="postBtn" />
+            <PaperAirplaneIcon className="postBtn" />
+          </div>
+          <BookmarkIcon className="postBtn" />
         </div>
-        <BookmarkIcon className="postBtn" />
-      </div>
+      )}
 
       {/* Caption */}
       <p className="p-5 truncate">
@@ -45,17 +90,28 @@ function Post({ id, username, userImg, postImg, caption }: IPost) {
       {/* Comments (Later on) */}
 
       {/* Input Box */}
-      <div>
-        <form className="p-4 flex items-center">
-          <FaceSmileIcon className="h-7" />
-          <input
-            type="text"
-            placeholder="Add a comment..."
-            className="flex-1 border-none focus:ring-0 outline-none"
-          />
-          <button className="font-semibo text-blue-400">Post</button>
-        </form>
-      </div>
+      {session && (
+        <div>
+          <form className="p-4 flex items-center">
+            <FaceSmileIcon className="h-7" />
+            <input
+              type="text"
+              value={comment}
+              placeholder="Add a comment..."
+              onChange={event => setComment(event.target.value)}
+              className="flex-1 border-none focus:ring-0 outline-none"
+            />
+            <button
+              type="submit"
+              disabled={!comment.trim()}
+              onClick={sendComment}
+              className="font-semibo text-blue-400"
+            >
+              Post
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
